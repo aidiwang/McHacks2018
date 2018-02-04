931f1344cc137346c0a78c851251326c52f3c6e2
jayohanna <33985435+jayohanna@users.noreply.github.com>
2018-02-04 09:33:32 -0500

    done

--- chatbot.py
+++ chatbot.py
@@ -4,9 +4,24 @@ import json
 from search import *
 from locate import *
 from translate import *
+import numpy as np
+import cv2
 
 
-def sendSparkGET(url):
+def sendSparkGETFILE(url):
+    """
+    This method is used for:
+        -retrieving message text, when the webhook is triggered with a message
+        -Getting the username of the person who posted the message if a command is recognized
+    """
+    request = urllib2.Request(url,
+                            headers={"Accept" : "application/json",
+                                     "Content-Type":"application/json"})
+    request.add_header("Authorization", "Bearer "+bearer)
+    contents = urllib2.urlopen(request)
+    return contents
+
+def sendSparkGETTEXT(url):
     """
     This method is used for:
         -retrieving message text, when the webhook is triggered with a message
@@ -28,7 +43,7 @@ def sendSparkPOST(url, data):
                             headers={"Accept" : "application/json",
                                      "Content-Type":"application/json"})
     request.add_header("Authorization", "Bearer "+bearer)
-    contents = urllib2.urlopen(request).read()
+    contents = urllib2.urlopen(request).read
     return contents
    
 @post('/')
@@ -43,53 +58,165 @@ def index(request):
     #retrieve files code here
     if webhook['data'].has_key('files'):
         for file_url in webhook['data']['files']:
-            response = sendSparkGET(file_url)
 
-            print "response starts"
+#            print "\n\nstart url"
+#            print file_url
+#            print "end\n\n"
+            
+            response = sendSparkGETFILE(file_url)
+
+            print "\n\nstart response"
+            #print response.info().get('Content-Disposition')
             print response
-            print "response ends"
+            print "end\n\n"
             
             content_disp = response.headers.get('Content-Disposition', None)
             if content_disp is not None:
                 filename = content_disp.split("filename=")[1]
                 filename = filename.replace('"', '')
-                with open(filename, 'w') as f:
+                
+                with open(filename, 'wb') as f:
                     f.write(response.read())
                     print 'Saved-', filename
+                '''with open(response.read(), 'rb') as f:
+                    data = f.read()
+                with open(filename, 'wb') as f:
+                    f.write(data)'''
+                
             else:
                 print "Cannot save file- no Content-Disposition header received."
-    else:
+    '''else:
         print "No files attached to retrieve!"
-        return "true"
+        return "true"'''
     #retrieve files code here
 
     print webhook['data']['id']
-    result = sendSparkGET('https://api.ciscospark.com/v1/messages/{0}'.format(webhook['data']['id']))
+    result = sendSparkGETTEXT('https://api.ciscospark.com/v1/messages/{0}'.format(webhook['data']['id']))
+
+    #result.read()
+    
     result = json.loads(result)
     msg = None
     if webhook['data']['personEmail'] != bot_email:
 
-        print "\n\nhi"
-        print result
-        print "end of result\n\n"
+        #print "\n\nhere are the results"
+        #print result
+        #print "end of result\n\n"
 
-        if 'files' in result:
-            in_image = result.get('files', '')
+        #if 'files' in result:
+        #    in_image = result.get('files', '')
         
-        in_message = result.get('text', '').lower()
+        in_message = result.get('text').lower()
         in_message = in_message.replace(bot_name, '')
         if 'hello' in in_message:
             msg = "Hello!"
         elif 'can you' in in_message or 'do you' in in_message or 'do something' in in_message:
-            msg = "I can do a Wiki search (search), find the location (locate) or translate the text (translate) of an image."
+            msg = "I can \n -do a Wiki search (type search + image) \n -find the location (type locate + image) \n -translate the text (type translate + image)\nof an image. "
         #elif 'batsignal' in in_message:
          #   print "NANA NANA NANA NANA"
           #  sendSparkPOST("https://api.ciscospark.com/v1/messages", {"roomId": webhook['data']['roomId'], "files": bat_signal})
+        
+        #start functions
+        #elif 'search' in in_message:
+        #    print "searching..."
+        #    msg = search(filename)
+            
+        #elif 'locate' in in_message:
+        #    msg = locate(filename)
+            
+        elif 'translate' in in_message:
+            tolanguage = result.get('text').split('translate')[1].strip(" ")
+            tolanguage = tolanguage.lower()
+
+
+            #dictionary?
+            languages = {
+                    "afrikaans": "af",
+                    "albanian" : "sq",
+                    "arabic" : "ar",
+                    "azerbaijani": "az",
+                    "basque": "eu",
+                    "bengali": "bn",
+                    "belarusian": "be",
+                    "bulgarian": "bg",
+                    "catalan": "ca",
+                    "chinese simplified": "zh-CN",
+                    "chinese traditional": "zh-TW",
+                    "croatian": "hr",
+                    "czech": "cs",
+                    "danish": "da",
+                    "dutch": "nl",
+                    "english": "en",
+                    "esperanto": "eo",
+                    "estonian": "et",
+                    "filipino": "tl",
+                    "finnish": "fi",
+                    "french": "fr",
+                    "galician": "gl",
+                    "georgian": "ka",
+                    "german": "de",
+                    "greek": "el",
+                    "gujarati": "gu",
+                    "haitian creole": "ht",
+                    "hebrew": "iw",
+                    "hindi": "hi",
+                    "hungarian": "hu",
+                    "icelandic": "is",
+                    "indonesian": "id",
+                    "irish": "ga",
+                    "italian": "it",
+                    "japanese": "ja",
+                    "kannada": "kn",
+                    "korean": "ko",
+                    "latin": "la",
+                    "latvian": "lv",
+                    "lithuanian": "lt",
+                    "macedonian": "mk",
+                    "malay": "ms",
+                    "maltese": "mt",
+                    "norwegian": "no",
+                    "persian": "fa",
+                    "polish": "pl",
+                    "portuguese": "pt",
+                    "romanian": "ro",
+                    "russian": "ru",
+                    "serbian": "sr",
+                    "slovak": "sk",
+                    "slovenian": "sl",
+                    "spanish": "es",
+                    "swahili": "sw",
+                    "swedish": "sv",
+                    "tamil": "ta",
+                    "telugu": "te",
+                    "thai": "th",
+                    "turkish": "tr",
+                    "ukrainian": "uk",
+                    "urdu": "ur",
+                    "vietnamese": "vi",
+                    "welsh": "cy",
+                    "yiddish": "yi",    
+                }
+
+            for k, m in languages.items():
+                if tolanguage.lower() == k:
+                    langcode = m
+
+            msg = translate(filename, langcode).encode('utf-8')
+            
+          
+        #end functions
+
         else:
             msg = "What can I do for you?"
         if msg != None:
             print msg
             sendSparkPOST("https://api.ciscospark.com/v1/messages", {"roomId": webhook['data']['roomId'], "text": msg})
+
+        #if my_function != None:
+         #   if "search" in my_function:
+          #  elif "locate" in my_function:
+           # elif ""
+
     return "true"
 
 bot_email = "jjla@sparkbot.io"
--- translate.py
+++ translate.py
@@ -1,2 +1,42 @@
-def translate():
-    return
+import Image, ImageEnhance, ImageFilter
+import pytesseract
+import argparse
+import cv2
+import os
+from googletrans import Translator
+
+def translate(translatefilename, langcode):
+
+    #pytesseract.pytesseract.tesseract_cmd = "C:\Program Files (x86)\Tesseract-OCR\tesseract.exe"
+    '''image=cv2.imread(translatefilename)
+    gray=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+
+    gray=cv2.threshold(gray,0,255,cv2.THRESH_BINARY|cv2.THRESH_OTSU)[1]
+    gray=cv2.medianBlur(gray,3)
+
+    filename='{}.png'.format(os.getpid())
+    cv2.imwrite(filename,gray)
+
+    text=pytesseract.image_to_string(Image.open(filename))
+    os.remove(filename)'''
+
+    im = Image.open(str(translatefilename))
+    im = im.filter(ImageFilter.MedianFilter())
+    enhancer = ImageEnhance.Contrast(im)
+    im = enhancer.enhance(2)
+    im = im.convert('1')
+    
+    text = pytesseract.image_to_string(im)
+
+    translator = Translator()
+    text = translator.translate(text, dest=langcode).text
+    
+    
+    return text
+    
+
+     
+    #show the output images
+    '''cv2.imshow("Image", image)
+    cv2.imshow("Output", gray)
+    cv2.waitKey(0)'''
